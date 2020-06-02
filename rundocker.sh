dda35823a4bd390de2e0418cb39467d1e4e6c3b8
john <john.b.y.yang@gmail.com>
2020-06-02 09:25:47 -0400

    m

--- rundocker.sh
+++ rundocker.sh
@@ -1,1 +1,1 @@
-docker rm -f fakeweb; docker run -d -v $PWD:/usr/src/app -p 8000:8000 --name fakeweb dk_tornado bash -c ' python app.py'
+docker rm -f fakeweb; docker run --restart=always  -d -v $PWD:/usr/src/app -p 8000:8000 --name fakeweb dk_tornado bash -c ' python app.py'
