57be37eacd19e66093406d4823853af7de737d7e
john <john.b.y.yang@gmail.com>
2020-10-28 09:02:53 +0800

    add docker log relo

--- rundocker.sh
+++ rundocker.sh
@@ -1,1 +1,1 @@
-docker rm -f fakeweb; docker run --restart=always  -d -v $PWD:/usr/src/app -p 8000:8000 --name fakeweb dk_tornado bash -c ' python app.py'
+docker rm -f fakeweb; docker run --log-driver json-file --log-opt max-size=10m --log-opt max-file=10 --restart=always  -d -v $PWD:/usr/src/app -p 8000:8000 --name fakeweb dk_tornado bash -c ' python app.py'
